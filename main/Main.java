42971feebfe4bca5c6910c31e562324a10105a2a
msengbusch <marek.sengbusch@flg-online.de>
2022-01-09 00:03:03 +0100

    wip

--- main/Main.java
+++ main/Main.java
@@ -1,3 +1,5 @@
+package main;
+
 import java.util.Objects;
 
 public class Main {
--- main/Test.java
+++ main/Test.java
@@ -1,3 +1,5 @@
+package main;
+
 public class Test {
     public static void main(String[] args) throws InterruptedException {
 /*        Engine engine = new Engine();
