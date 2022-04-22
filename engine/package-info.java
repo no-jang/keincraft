84f421455fd996071e4a8c792c3f192585d03057
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-04-22 11:18:10 +0200

    chore: remove all

--- engine/package-info.java
+++
@@ -1,6 +0,0 @@
-@DefaultQualifier(value = NonNull.class, locations = {TypeUseLocation.RETURN, TypeUseLocation.FIELD, TypeUseLocation.PARAMETER})
-package engine;
-
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.framework.qual.DefaultQualifier;
-import org.checkerframework.framework.qual.TypeUseLocation;
